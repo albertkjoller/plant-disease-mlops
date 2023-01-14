3e8cdb173214716f6d095b804dbfe81e5d90b1d4
Andreas Theilgaard <actheilgaard@gmail.com>
2023-01-14 12:17:31 +0100

    edit v3

--- tests/test_api.py
+++ tests/test_api.py
@@ -24,22 +24,22 @@ app = create_app()
 client = TestClient(app)
 
 # Function 1: '/' Method GET
-def func1(client):
+def test_func1(client):
     response = client.get('/')
     assert response.status_code == 200
 
 # Function 2: '/upload_model' Method: POST
-def func2(client):
+def test_func2(client):
     response = client.post('/upload_model')
     assert response.status_code == 200
 
 # Function 3: '/load_model' Method: POST
-def func3(client):
+def test_func3(client):
     response = client.post('/load_model')
     assert response.json()['model']['loaded'] == True
 
 # Function 4: '/predict' Method: POST
-def func4(client):
+def test_func4(client):
     load_model=response = client.post('/load_model') # start by loading model
     response = client.post('/predict')
     assert response.json()['output']['results']['A']['0']['pred']==15

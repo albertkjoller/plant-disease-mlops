b9ef7caa40ed16698b1e0372ab0e381a49c274e7
Andreas Theilgaard <actheilgaard@gmail.com>
2023-01-14 12:36:04 +0100

    update v4

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,31 +15,28 @@ a=str(path.parent.absolute())
 sys.path.append(a)
 from app.main import router
 ##############################
-
 from app.app_setup import create_app
-from tests import _PATH_DATA
-
 
 app = create_app()
 client = TestClient(app)
 
 # Function 1: '/' Method GET
-def test_func1(client):
+def test_func1():
     response = client.get('/')
     assert response.status_code == 200
 
 # Function 2: '/upload_model' Method: POST
-def test_func2(client):
+def test_func2():
     response = client.post('/upload_model')
     assert response.status_code == 200
 
 # Function 3: '/load_model' Method: POST
-def test_func3(client):
+def test_func3():
     response = client.post('/load_model')
     assert response.json()['model']['loaded'] == True
 
 # Function 4: '/predict' Method: POST
-def test_func4(client):
+def test_func4():
     load_model=response = client.post('/load_model') # start by loading model
     response = client.post('/predict')
     assert response.json()['output']['results']['A']['0']['pred']==15
